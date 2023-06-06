ab14ba36dff7bb989223827cc65cee59f3ef9dc6
Koome <judekoome360@gmail.com>
2023-06-06 21:46:22 +0300

    Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+    def initialize
+        @knowledge = []
+     end
+     def learn vale
+        @knowledge << vale
+     end
+     def knowledge
+        @knowledge
+     end
 end
